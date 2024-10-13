2869ba3dc0469a0e150b194529017ea9621d44c1
Muthuswamy <muthuswamynadar25@gmail.com>
2024-10-13 19:55:33 +0530

    the loop is working

---
+++ Fundamentals/ControlStructure.py
@@ -0,0 +1,57 @@
+
+# if statements
+
+x= 5
+if x < 10:
+    print("x is greater than 10")
+
+
+# if else statments
+
+x=5
+if x > 10:
+    print(f"the value is greater than {x}")
+else:
+    print(f"the value is less than {x} ")
+
+# if elif statements
+
+x=15
+if x> 30:
+    print("X is greater ") 
+elif x<30:
+    print(f"the value is greter {x}")
+else:
+    print("Everthing is fine")
+
+#loops
+
+fruits={"apple","banna","Cherry","mango"}
+for fruit in fruits:
+    print(fruit)
+
+Laptops={"Acer","Lenovo","Mac","Dell"}
+for laps in Laptops:
+    print(laps)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
